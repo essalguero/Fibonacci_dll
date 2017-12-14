1052555456931066fee703c883686d3845dfafeb
LIVEU-TAD\eugenio.salguero <eugenio.salguero@live.u-tad.com>
2017-12-14 20:43:16 +0100

    no message

--- Fibonacci_dll/Fibonacci_dll.h
+++ Fibonacci_dll/Fibonacci_dll.h
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-extern "C" int fibonacci(int n) {
+extern "C" double fibonacci(int n) {
 	int c;
-	int next = 0;
-	int first = 0;
-	int second = 1;
+	double next = 0;
+	double first = 0;
+	double second = 1;
 
 	for (c = 0; c <= n; c++)
 	{
@@ -24,9 +24,43 @@ extern "C" int fibonacci(int n) {
 	return next;
 }
 
-extern "C" int fibonacci_recursivo(int n) {
+extern "C" double fibonacci_recursivo(int n) {
 	if (n == 0 || n == 1)
 		return n;
 	else
 		return fibonacci_recursivo(n - 2) + fibonacci_recursivo(n - 1);
+}
+
+extern "C" int fibonacci_DivideAndConquer(int n) {
+	if (n <= 0)
+		return 1;
+
+	int i = n - 1;
+	double auxOne = 0;
+	double auxTwo = 1;
+
+	double a = auxTwo;
+	double b = auxOne;
+
+	double c = auxOne;
+	double d = auxTwo;
+
+	while (i > 0) {
+		if (i % 2 != 0) {
+			auxOne = ((d * b) + (c * a));
+			auxTwo = ((d * (b + a)) + (c * b));
+
+			a = auxOne;
+			b = auxTwo;
+		}
+
+		auxOne = (c * c) + (d * d);
+		auxTwo = (d * ((2 * c) + d));
+		c = auxOne;
+		d = auxTwo;
+
+		i = i / 2;
+	}
+
+	return a + b;
 }
